accab2fd888763d7c4e3f6524c629dd6a37d9be8
yash khandelwal <105146802+yashgeek@users.noreply.github.com>
2023-11-22 13:02:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 import inquirer from 'inquirer';
-import qr from "qr-image"
-import fs from "fs"
+import qr from 'qr-image';
+import fs from 'fs';
 
 inquirer
     .prompt([
         /* Pass your questions in here */
         {
-            "message": "Type in URl: ",
-            "name": "URL"
-        }
+            message: 'Type in URL: ',
+            name: 'URL',
+        },
     ])
     .then((answers) => {
-        // Use user feedback for... whatever!!
-        // console.log(answers)
-        const url = answers.URL
-        var qr_svg = qr.image(url);
+        const url = answers.URL;
+
+        // Generate QR code
+        const qr_svg = qr.image(url);
         qr_svg.pipe(fs.createWriteStream('qr_img.png'));
 
-        
+        // Save URL to a text file
+        fs.writeFile('URL.txt', url, (err) => {
+            if (err) throw err;
+            console.log('The URL has been saved to URL.txt');
+        });
     })
     .catch((error) => {
         if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
+            console.error('Prompt couldn\'t be rendered in the current environment');
         } else {
-            // Something else went wrong
+            console.error('Something else went wrong');
         }
     });
-
